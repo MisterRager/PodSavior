58fafb100fa32b135f870090cc2df99534dddcb8
Alan Rager <alan.rager@gmail.com>
2018-12-16 22:04:09 +0000

    first post

---
+++ app/src/main/java/es/lolrav/podsavior/HostActivity.kt
@@ -0,0 +1,11 @@
+package es.lolrav.podsavior
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
