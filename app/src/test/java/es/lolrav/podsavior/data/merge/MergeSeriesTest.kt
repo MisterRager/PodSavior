5de0678f6c3f834691b6264fdcd710ec6994f9ba
Rager <alan.rager@gmail.com>
2019-03-09 09:46:25 -0800

    add more test

---
+++ app/src/test/java/es/lolrav/podsavior/data/merge/MergeSeriesTest.kt
@@ -0,0 +1,84 @@
+package es.lolrav.podsavior.data.merge
+
+import com.squareup.moshi.Moshi
+import dagger.Component
+import es.lolrav.podsavior.database.entity.Series
+import es.lolrav.podsavior.di.NetModule
+import es.lolrav.podsavior.di.TestSeriesModule
+import es.lolrav.podsavior.net.itunes.entity.ITunesSearchResults
+import okio.Okio
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import javax.inject.Inject
+
+class MergeSeriesTest {
+    @Inject
+    lateinit var seriesA: Series
+    @Inject
+    lateinit var seriesB: Series
+    @Inject
+    lateinit var merger: MergeSeries
+
+    @Before
+    fun setup() {
+        DaggerMergeSeriesTestComponent.builder().build().inject(this)
+    }
+
+    @Test
+    fun testMatchOnUid() {
+        assertTrue(merger.matches(seriesA, seriesA))
+        assertTrue(merger.matches(seriesB, seriesB))
+        assertTrue(merger.matches(seriesA, seriesA.copy(name = "updated")))
+
+        assertFalse(merger.matches(seriesA, seriesB))
+        assertFalse(merger.matches(seriesB, seriesA))
+    }
+
+    @Test
+    fun `test that new data does not overwrite subscription status`() {
+        val unsubscribedUnsaved = seriesA.copy(isSaved = false, isSubscribed = false)
+        val savedAndSubscribed = seriesA.copy(isSaved = true, isSubscribed = true)
+
+        val updateWithSaved = merger.merge(unsubscribedUnsaved, savedAndSubscribed)
+
+        assertTrue(updateWithSaved.isSaved)
+        assertTrue(updateWithSaved.isSubscribed)
+
+        val updateAgainWithUnsaved = merger.merge(updateWithSaved, unsubscribedUnsaved)
+
+        assertTrue(updateAgainWithUnsaved.isSaved)
+        assertTrue(updateAgainWithUnsaved.isSubscribed)
+
+        val unsubscribeWithSaved = merger.merge(
+                updateAgainWithUnsaved,
+                updateAgainWithUnsaved.copy(isSubscribed = false))
+
+        assertTrue(unsubscribeWithSaved.isSaved)
+        assertFalse(unsubscribeWithSaved.isSubscribed)
+    }
+
+    @Test
+    fun `full-on integration test`() {
+        val moshi: Moshi = NetModule.providesMoshi()
+
+        val parsed: ITunesSearchResults = moshi
+                .adapter(ITunesSearchResults::class.java)
+                .fromJson(Okio.buffer(Okio.source(
+                        javaClass.classLoader!!
+                                .getResourceAsStream("america_itunes.json"))))!!
+
+        merger.merge(parsed.results, parsed.results[0].copy(isSaved = true, isSubscribed= true))
+
+    }
+}
+
+@Component(modules = [TestSeriesModule::class])
+interface MergeSeriesTestComponent {
+    @Component.Builder
+    interface Buidler {
+        fun build(): MergeSeriesTestComponent
+    }
+
+    fun inject(test: MergeSeriesTest)
+}
